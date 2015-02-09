8173405404de4d0b925a25606b1cb6ec5f113223
ateel <ateel@engr-134-11.eastern.ewu.edu>
2015-02-09 13:42:55 -0800

    midterm part 2

--- 2/js/scripts.js
+++ 2/js/scripts.js
@@ -2,3 +2,15 @@
 // Show Date
 // Show Number of Messages in Inbox
 // Add JavaScript to close button
+
+var msg1 = "Promotions";
+var msg2 = "Better with more stuff!";
+
+var total = 100+4561;
+$('.content p').text('Unread Messages:' + total);
+
+$('.header').text(msg1);
+
+$('.message .close').on('click', function() {
+  $(this).closest('.message').fadeOut();
+});
